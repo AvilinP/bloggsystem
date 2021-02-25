fb5b76ab02bc2dc5871b138c2252e7d8747b40de
Avilin <avilin.persson@gmail.com>
2021-02-25 15:35:36 +0100

    fixed register - failed input.

--- views/register.php
+++ views/register.php
@@ -1,3 +1,8 @@
+<?php
+// Start the session
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INDEX</title>
+    <title>REGISTER</title>
 
     <link rel="stylesheet" href="style.css">
 
@@ -20,16 +25,52 @@
     $password = "";
     $pdo = new PDO($dsn, $user, $password);
 
-   ?> 
+if(isset($_POST['register'])) 
+{ 
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
 
+// Verify required input fields when register
+
+if(empty($_POST["name"]) || empty($_POST["username"]) || empty($_POST["password"]))
+{
+    // Must fix error message on register.php 
+    $message = '<label> All fields are required to register</label>';
+    
+} else {
+
+    $sql = "INSERT INTO users (name, username, password) VALUES(:name_IN, :username_IN, :password_IN) ";
+    $stm = $pdo->prepare($sql);
+    $stm->bindParam(':name_IN', $name);
+    $stm->bindParam(':username_IN', $username);
+    $stm->bindParam(':password_IN', $password);
+
+      if($stm->execute()) {
+        header("location:login.php");
+      } else {
+        echo "Something went wrong!";      
+        }
+
+}
+
+?>
+
+
+<?php 
+    if(isset($message)) {
+        echo '<label>'.$message.'<label>';
+    } 
+?>
 
     <!-- FORMS  --> 
     <h2> REGISTER HERE! </h2>
-    <form method="POST" action="handleRegister.php">
+    <form method="POST" >
         <input type="text" placeholder="Your name..." name="name"> <br>
         <input type="text" placeholder="Your username..." name="username"> <br>
         <input type="password" placeholder="Your password..." name="password"> <br>
-        <input type="submit" value="Register" class="register-btn">
+        <input type="submit" value="Register" name="register" class="register-btn">
     </form>
     
 </body>

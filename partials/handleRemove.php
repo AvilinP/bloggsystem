e53e42db0414f047d580d1be81f2e4e2775f0339
shawndevp <shawn.ornerstedt99@hotmail.com>
2021-03-01 16:58:12 +0100

    handles and fix

---
+++ partials/handleRemove.php
@@ -0,0 +1,17 @@
+                   <!-- /////// PHP FOR REMOVING COMMENTS BY ID INTO WEBSITE. -->
+
+<?php 
+session_start();
+include("../views/db.php");
+
+
+$idRemove = $_GET['id'];
+$stm = $pdo->query("DELETE FROM comments WHERE id=$idRemove");
+
+if($stm->execute()) {
+    header("location:../views/loggedin.php");
+}
+else {
+    echo "Det gick fel!";
+}
+?>

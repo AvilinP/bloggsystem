e0f7dccc28b79193ddfbb69ee29d2e9a2303bdc2
Avilin <avilin.persson@gmail.com>
2021-03-08 13:26:55 +0100

    flex/responsive on main.php

--- main.php
+++ main.php
@@ -24,40 +24,32 @@ session_start();
 
         <img id="blog-nav-logo" src="uploads/Millhouse_logo.png" alt="Millhouse Logo">
 
-        <!-- <a id="blog-nav-a" href="views/register.php">Register/Login</a> -->
-
         <?php
 
         // if you're loggedin admin or user
         if(isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_id']  != "")
         {
             echo '<p id="blog-welcome-p">Welcome '.$_SESSION['sess_name'] . ' ' . '<a href="views/logout.php">- logout</a> </p>';
-           // echo '<h4><a href="views/logout.php">Logout</a></h4>';
         
         ?>
     
     </nav>
 
 
-    <div id="main-blog-posts"> 
+    <div id="main-blog-posts-container"> 
 
     <?php
-
-            // Shows posts from loggedin.php
-            $stm = $pdo->query("SELECT id, username, comment FROM comments");
-            while($row = $stm->fetch()) 
-                echo $row['username'] . " " . $row['comment'] . "<br />";
 
         } else { 
         
             echo '<p id="blog-welcome-p"> You\'re not logged in. Please log in/register ' . ' ' . '<a href="views/register.php">- here</a> </p>';
             die();
         }
-
-        $stm = $pdo->query("SELECT image, description FROM posts");
+            // Shows posts from loggedin.php
+            $stm = $pdo->query("SELECT image, description FROM posts");
             while($row = $stm->fetch()) {
-                echo '<img src="../bloggsystem' . $row['image'] . '" height=300 /><br />';
-                echo $row['description'];
+                echo '<div id="main-blog-posts"> <img src="../bloggsystem' . $row['image'] . '" height=200 /><br />';
+                echo '<p id="main-blog-posts-p">' . $row['description'] . '</p>' . '</div>';
             }
         ?>
 
